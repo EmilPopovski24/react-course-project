b1ad6d668261130027aec1c4df6f5fb5a86adda2
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-11-27 19:36:27 +0200

    update Account.js

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -4,7 +4,7 @@ export const Account = () => {
     return (
             <div>  
                 <h3>My Account</h3>
-                <h5>Personal Information</h5>
+                <h5>Personal Information:</h5>
             </div>
     )
 }
