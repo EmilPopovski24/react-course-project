2b7d0a2fab71d5fbd9e1f04b1d86f95a1e962055
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-12-02 15:33:08 +0200

    update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 export const Footer = () => {
     return(
         <div>
-            <h4 className="footer">All rights reserved &#169; 2023
+            <h4 className="footer">All rights reserved &#169; 2023-2024
              </h4>
         </div>
     );
