54147ae559026e94661eb201bb5fa2f4c018177e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-18 19:23:48 +0200

    update Home view

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,8 +4,8 @@ export const Home = () => {
     return (
         <section className="homepage">
             <div className="welcome-page">
-                <h1>Welcome to MovieRate</h1>
-                <h5>The unofficial Movie social media</h5>
+                <h1 className="welcome-message">Welcome to MovieRate</h1>
+                <h5 className="welcome-message">The unofficial Movie social media</h5>
             </div>
             <div className="cinema">
                 <img src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2luZW1hfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="cinema" className="start-pic"/>
