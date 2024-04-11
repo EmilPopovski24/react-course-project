3f3b422cda3f9003b6a98e568df04e516bb0a656
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-11 18:40:22 +0300

    Update Account.js

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,11 +1,15 @@
+import { useContext } from "react";
 import "./Account.css";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export const Account = () => {
+
+    const user = useContext(AuthContext);
 
     return (
             <div>  
                 <h3 className="profile-header">My Account</h3>
-                <p>name: {userEmail} </p>
+                <p>name: {user.userEmail} </p>
                 <h5 className="profile-info">Personal Information</h5>
             </div>
     )
