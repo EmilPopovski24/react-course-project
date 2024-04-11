453af5338e9ef3e9027e13f06a41548fa4a1c3be
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-11 18:33:46 +0300

    Update Account.js

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,10 +1,13 @@
 import "./Account.css";
 
-export const Account = () => {
-    
+export const Account = ({
+    username
+}) => {
+
     return (
             <div>  
                 <h3 className="profile-header">My Account</h3>
+                <p>name: {username} </p>
                 <h5 className="profile-info">Personal Information</h5>
             </div>
     )
