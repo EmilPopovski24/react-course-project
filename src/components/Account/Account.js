736bb010fc8e3bccaab39e960dfb602113115940
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-07 18:48:49 +0300

    Footer update

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -7,7 +7,7 @@ export const Account = () => {
     const user = useContext(AuthContext);
 
     return (
-            <div>  
+            <div className="accpunt-page">  
                 <h3 className="profile-header">My Account</h3>
                 <p>name: {user.userEmail} </p>
                 <h5 className="profile-info">Personal Information</h5>
