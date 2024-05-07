7497896a03429285830b4d55cc797b9e09c452f1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-07 19:47:40 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import "./Home.css";
 
 export const Home = () => {
     return (
+        <>
         <section className="homepage">
             <div className="welcome-page">
                 <h1 className="welcome-message">Welcome to MovieRate</h1>
@@ -11,5 +12,6 @@ export const Home = () => {
                 <img src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2luZW1hfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="cinema" className="start-pic"/>
             </div>
         </section>
+        </>
     );
 }
