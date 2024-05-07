5d1fb7368356e800cfec5b45a70286927be9f16e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-07 19:49:54 +0300

    Update Catalog.js

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -4,12 +4,15 @@ import { CatalogItem } from "./CatalogItem/CatalogItem";
 export const Catalog = ({
     movies,
 }) => {
+
     return(
+        <>
         <section className="catalog">
             {movies.length === 0 ?
                 <h2 className="catalog-header">No Movies For Review</h2>
             :   <h2 className="catalog-header">Currently Reviewed Movies</h2>}
             {movies.map(x => <CatalogItem key={x._id} {...x} />)}         
         </section>
+        </>
     );
 }
