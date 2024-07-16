dca13f5a055d496a163362517c2d219b295b4454
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-16 18:55:52 +0300

    Update AddComment.js

--- src/components/Details/AddComment/AddComment.js
+++ src/components/Details/AddComment/AddComment.js
@@ -3,7 +3,8 @@ import "./AddComment.css"
 
 export const AddComment = ({
     onCommentSubmit,
-    username
+    username,
+    comment, setComment
 }) => {
     
     const { values, changeHandler, onSubmitfunc } = useForm({
@@ -15,7 +16,7 @@ export const AddComment = ({
         <article className="addcomment">
             <h4>Add your comment:</h4>
                 <form className="form" onSubmit={onSubmitfunc} >
-                    <textarea name ="comment" className="comment-area" placeholder="Your comment..." value={values.comment} onChange={changeHandler}></textarea>
+                    <textarea name ="comment" className="comment-area" placeholder="Your comment..." value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
                     <input className ="submit-btn"  type="submit" value="Publish" />
                 </form>                       
         </article>
