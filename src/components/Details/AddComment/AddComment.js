44f45c9791d2eaa87ad006fc0bd1175cace9d387
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-17 19:42:33 +0300

    Update AddComment.js

--- src/components/Details/AddComment/AddComment.js
+++ src/components/Details/AddComment/AddComment.js
@@ -16,7 +16,7 @@ export const AddComment = ({
         <article className="addcomment">
             <h4>Add your comment:</h4>
                 <form className="form" onSubmit={onSubmitfunc} >
-                    <textarea name ="comment" className="comment-area" placeholder="Your comment..." value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+                    <textarea name ="comment" className="comment-area" placeholder="Your comment..." value={values.comment} onChange={(e) => setComment(e.target.value)}></textarea>
                     <button className='post-btn' type="submit">Add comment</button>
                 </form>                       
         </article>
