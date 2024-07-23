a74614c97e5606e16d9244b20e7097e1b4cfe95a
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-23 13:32:51 +0300

    Update movieService.js

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,52 +1,36 @@
 import { requestFactory } from "./requester";
 
-const baseUrl = "http://localhost:3030/data/";
+const baseUrl = "http://localhost:3030/data/movies";
 
 export const movieServiceFactory = (token) => {
     const request = requestFactory(token);
 
     const editMovie = (movieId, data) => {
-        const result = request.put(`${baseUrl}/movies/${movieId}`, data)
+        const result = request.put(`${baseUrl}/${movieId}`, data)
         return result;
     }
 
     const getAllMovies = async () => {
-        const result = await request.get(`${baseUrl}/movies`);
+        const result = await request.get(baseUrl);
         const movies = Object.values(result);
         return movies
     }
     
     const getOneMovie = async (movieId) => {
-        const result = await request.get(`${baseUrl}/movies/${movieId}`);
-
+        const result = await request.get(`${baseUrl}/${movieId}`);
         return result;
     }
     
     const createMovie = async (movieData) => {
-        const res = await request.post(`${baseUrl}/movies`, movieData);
+        const res = await request.post(baseUrl, movieData);
         return res;
     }
-
-    const createComment = async (movieId, commentData) => {
-        const result = await request.post(`${baseUrl}/comments`,{movieId, commentData});
-        return result
-    }
-    
-    const getAllComments = async (movieId) => {
-        const query = encodeURIComponent(`movieId="${movieId}"`)
-        const author = encodeURIComponent(`author=_ownerId:users`);
-        const result = await request.get(`${baseUrl}/comments/?where=${query}&load=${author}`);
-        // const comments = Object.values(result)
-        return  result
-    }
-    
+   
     return {
         editMovie,
         getAllMovies,
         getOneMovie,
-        createMovie,
-        createComment,
-        getAllComments
+        createMovie
     };
 
 }
