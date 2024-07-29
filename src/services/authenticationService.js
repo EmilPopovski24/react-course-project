7d47ab9b77129a3a317aa1d2ec5fc1122367c4a8
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-29 14:00:05 +0300

    Update authenticationService.js

--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -4,6 +4,7 @@ const usersUrl = "http://localhost:3030/users"
 
 export const authServiceFactory = (token) => {
     const request = requestFactory(token);
+    
     return {
         login:(loginData) => request.post(`${usersUrl}/login`, loginData),
         register: (registerData) =>  request.post(`${usersUrl}/register`, registerData),
