6a2099495ee69c10ae4a0a711a396014b310adbb
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-31 19:32:38 +0300

    Update useService.js

--- src/hooks/useService.js
+++ src/hooks/useService.js
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 export const useService = (serviceFactory) => {
-    
     const { token } = useContext(AuthContext)
     const service = serviceFactory(token);
     return service
