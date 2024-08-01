33a32e54cc567b28f562d983166c9adea69902eb
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-01 19:17:22 +0300

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -19,16 +19,16 @@ export const Register = () => {
                 <h1 className="register-header">Register</h1>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input name="email" value={values.email} onChange={changeHandler} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input name="email" value={values.email} onChange={changeHandler} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input name="password" value={values.password} onChange={changeHandler} type="password" className="form-control" id="exampleInputPassword1" />
+                    <input name="password" value={values.password} onChange={changeHandler} type="password" className="form-control" id="exampleInputPassword1" required/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Confirm Password</label>
-                    <input name="confirm-password" value={values["confirm-password"]} onChange={changeHandler} type="password" className="form-control" id="confirm-password" />
+                    <input name="confirm-password" value={values["confirm-password"]} onChange={changeHandler} type="password" className="form-control" id="confirm-password" required/>
                 </div>      
                 <button style={{background:"green", border:"none"}} type="submit" className="btn btn-primary">Register</button>
                 <div className="loginoption">
