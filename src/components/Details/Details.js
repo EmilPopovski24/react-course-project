43fb230bc446c36a8f66d62ca6bfba3eea0fe12a
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-11-03 13:32:46 +0200

    Update Details.js

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -24,43 +24,43 @@ export const Details = ({
     const [rates, setRates] = useState([]);
     const navigator = useNavigate();
 
-    useEffect(()=> {
-        movieService.getOneMovie(movieId)
-            .then(result => {
-                setMovie(result)
-                return commentService.getAllComments(movieId)
-                // rateService.getAllRates(movieId)
-            })
-            .then(result => {
-                setMovie(result)
-                return rateService.getAllRates(movieId)
-            })
-            .then(result => {
-                setComments(result);
-                setRates(result)
-            })
-    }, [movieId]);
-
-    // useEffect(() => {
-    //     Promise.all([
-    //         movieService.getOneMovie(movieId),
-    //         commentService.getAllComments(movieId),
-    //         rateService.getAllRates(movieId)
-    //     ])
-    //     // .then(result => {
-    //     //     setComments(result);
-    //     //     setRates(result)
-    //     // })
-    //     // }, [movieId]);
-
-    //     .then(([movieData, comments, rates]) => {
-    //         setMovie({
-    //             ...movieData,
-    //             comments, 
-    //             rates,
+    // useEffect(()=> {
+    //     movieService.getOneMovie(movieId)
+    //         .then(result => {
+    //             setMovie(result)
+    //             return commentService.getAllComments(movieId)
+    //             // rateService.getAllRates(movieId)
     //         })
-    //     })
-    // },[movieId])
+    //         .then(result => {
+    //             setMovie(result)
+    //             return rateService.getAllRates(movieId)
+    //         })
+    //         .then(result => {
+    //             setComments(result);
+    //             setRates(result)
+    //         })
+    // }, [movieId]);
+
+    useEffect(() => {
+        Promise.all([
+            movieService.getOneMovie(movieId),
+            commentService.getAllComments(movieId),
+            rateService.getAllRates(movieId)
+        ])
+        // .then(result => {
+        //     setComments(result);
+        //     setRates(result)
+        // })
+        // }, [movieId]);
+
+        .then(([movieData, comments, rates]) => {
+            setMovie({
+                ...movieData,
+                comments, 
+                rates,
+            })
+        })
+    },[movieId])
 
     const onCommentSubmit = async (e) => {      
 
